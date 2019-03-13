7d8663f9a8076b495569e54d91b2d671e06bbd73
TrindadeThiago <thiago@itaqua.org>
2019-03-13 14:03:04 -0300

    Conclusao do arquivo rodizio.php e alteracoes do layout

--- php/rodizio.php
+++ php/rodizio.php
@@ -1,16 +1,17 @@
 <?php
     if(!empty($_POST['optPlaca'])){
         $optPlaca = $_POST['optPlaca'];
-    }
-    
-    if(!empty($optPlaca)){
-        switch($optPlaca){
-            case "1" || "2":
-                echo "Segunda-Feira";
-                break;
-            case "3" || "4":
-                echo "Terca-feira";
-                break;
+
+        if($optPlaca == 1 || $optPlaca == 2){
+            echo "Segunda-feira";
+        }else if($optPlaca == 3 || $optPlaca == 4){
+            echo "Terca-feira";
+        }else if($optPlaca == 5 || $optPlaca == 6){
+            echo "Quarta-feira";
+        }else if($optPlaca == 7 || $optPlaca == 8){
+            echo "Quinta-feira";
+        }else if($optPlaca == 9 || $optPlaca == 10){
+            echo "Sexta-feira";
         }
-    }    
+    }  
 ?>
